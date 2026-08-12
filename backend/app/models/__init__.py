dfb9b72dbc56818475af3422660f75495fcb320c
Gopiga-2006 <gopigar10hrsk@gmail.com>
2026-08-12 19:08:25 +0530

    feat(data): register ORM models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.user import User
+from app.models.department import Department
+from app.models.exit_request import ExitRequest
+from app.models.approval import Approval
+from app.models.clearance_task import ClearanceTask
+
+__all__ = ["User", "Department", "ExitRequest", "Approval", "ClearanceTask"]
