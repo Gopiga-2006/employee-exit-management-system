c2c07aa8fc53af416f998c42358710788a39883f
Gopiga-2006 <gopigar10hrsk@gmail.com>
2026-09-17 21:54:10 +0530

    fix(cors): allow deployed frontend

--- app/main.py
+++ app/main.py
@@ -15,7 +15,11 @@ app = FastAPI(title="Employee Exit Management System", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://employee-exit-management-system.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
